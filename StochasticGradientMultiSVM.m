3781ef4651e39a25776677ab3a702fe52f5c95ba
jaylohokare <jaylohokare@gmail.com>
2018-03-11 19:01:59 -0400

    fixed Bugs

--- StochasticGradientMultiSVM.m
+++ StochasticGradientMultiSVM.m
@@ -2,91 +2,80 @@ load q3_1_data.mat
 
 C=100;
 options.iterations=1;
+options.k = 2;
 options.eta0 = 1;
 options.eta1 = 100;
 options.C = C;
 [options.featureCount,~] = size(trD);
 
 
-%[w,objective] = SGMSVM(trD, trLb, options);
 
-function [W, obj] = SGMSVM(X, Y, options)
+
+[w,obj] = SGMSVM(trD, trLb, options)
+
+
+function [W, objective] = SGMSVM(X, Y, options)
 labels = Y;
 data = X;
 C = options.C;
 iterations = options.iterations;
 n_zero = options.eta0;
 n_one = options.eta1;
-n = size(X, 1);
+
+d = size(X,1)
+n = size(X, 2);
+
 k = options.featureCount;
 
-W = zeros(size(X, 2));
-loss = size(X, 2);
-objective = zeros(iterations);
-w_dim = size(W, 2);
+W = zeros(d, k);
+
+loss = size(X, 1);
+
+objective = zeros(iterations, 1);
 
 
 
 for epoch = 1:iterations
     n_res = n_zero / (n_one + epoch);
-    indx = randperm(size(X, 1));
+    %indx = randperm(size(data, 1));
+    j = rand(size(X,2));
     
-    for i = indx
-        newW = W;
-        newW(:,(i)) = -inf;
+    %for j = indx
         
-        argmax = newW' * X(:,i)
         
-        [~, yi_hat_index] = max(argmax);
-        yi_hat = yi_hat_index;
+       for i = 1:n   
+           newW = W;      
+           newW(:,i) = -inf;    
+           [~, yi_hat_index] = max(newW' * X(:,i));
         
-        %w_tmp = W(:,yi_hat)';
+           
+           condition = W(:, i)' * X(:,i) - W(:,yi_hat_index)' * X(:,i) + 1;
         
-        condition = W(:,yi_hat)' * X(:,i) - W(:,(i))' * X(:,i) + 1;
-        
-        loss(i) = max(condition, 0);
-        
-        for j = 1:k
-           if j == (i)
+           if i == j
+                if condition > 0
+                    W(:,i) = W(:,i) - n_res * (W(:,i) / n);
+                else
+                    W(:,i) = W(:,i) - n_res * ( W(:,i) / n - C * X(:,i));
+                end
+           elseif i == yi_hat_index
                if condition > 0
-                   if size(W, 2) == 1
-                        W(j) = W(j) - n_res * (sum(W')' / n - C * X(:, i));
-                   else
-                        W(:,j) = W(:,j) - n_res * (sum(W')' / n - C * X(:, i));
-                   end
-               else
-                   if size(W, 2) == 1
-                        W(j) = W(j) - n_res * (sum(W')' / n);
-                   else
-                        W(:,j) = W(:,j) - n_res * (sum(W')' / n);
-                   end
-               end
-           elseif j == yi_hat
-               if condition > 0
-                   if size(W, 2) == 1
-                        W(j) = W(j) - n_res * (sum(W')' / n +  C * X(:, i));
-                   else
-                        W(:,j) = W(:,j) - n_res * (sum(W')' / n +  C * X(:, i));
-                   end
-               else
-                   if size(W, 2) == 1
-                        W(j) = W(j) - n_res * (sum(W')' / n);
-                   else
-                        W(:,j) = W(:,j) - n_res * (sum(W')' / n);
-                   end
+                    W(:,i) = W(:,i) - n_res * (W(:,yi_hat_index) / n);
+                else
+                    W(:,i) = W(:,i) - n_res * ( W(:,yi_hat_index) / n + C * X(:,i));
                end
            else
-               W(:,j) = W(:,j) - n_res * (sum(W')' / n);
+               W(:,i) = W(:,i) - n_res * (W(:,i) / n);
+               
            end
-        end
-    end
-    if w_dim ~= 2
-        Y_tr = X' * W;
-    else
-        Y_tr = X' * W(:,1);
-    end
-
-    objective(epoch) = 1/2 * sum(sum(W'*W)) + C * sum(loss);
-end
+               
+           
+           
+       end
+       
+       
+ 
 end
 
+end
+   
+    
